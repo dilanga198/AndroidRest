9edcd7312b8c97742ccfb5753d13e56e3d00c92e
nithbtns <tharindu32@gmail.com>
2013-10-09 17:32:38 +0530

    rest api with update task

--- rest/src/main/java/sun/com/UpdateTask.java
+++ rest/src/main/java/sun/com/UpdateTask.java
@@ -1,5 +1,17 @@
 package sun.com;
 
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+
 /**
  * Created with IntelliJ IDEA.
  * User: nimantha
@@ -7,6 +19,41 @@ package sun.com;
  * Time: 4:39 PM
  * To change this template use File | Settings | File Templates.
  */
+@Path("/update_task")
 public class UpdateTask {
+
+    @Context
+    UriInfo uriInfo;
+
+    @Context
+    Request request;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/user/{taskObject}")
+    public String respondAsReady(@PathParam("taskObject") String updateTaskJsonObj) {
+
+        Gson gson = new Gson();
+        Task updateTask = gson.fromJson(updateTaskJsonObj, Task.class);
+        Boolean found=false;
+        for(Task task:TaskList.taskList){
+            if(task.getId().equals(updateTask.getId())){
+                task.setDescription(updateTask.getDescription());
+                task.setExpire(updateTask.getExpire());
+                task.setSrartedAt(updateTask.getSrartedAt());
+                task.setStatus(updateTask.getStatus());
+
+                found=true;
+
+            }
+
+            if(found){
+            break;
+            }
+        }
+
+        return gson.toJson(updateTask);
+
+    }
 
 }
